ae4f1e1698406c430e836f393496ecfa0a61814d
Kevin Rutherford <kevin@rutherford-software.com>
2014-07-09 20:26:08 +0100

    Add a bubble chart showing commit sizes per developer

--- public/chart.js
+++ public/chart.js
@@ -1,3 +1,36 @@
+
+function sizes_plot(target, data) {
+    var series = [];
+    $.each(data, function(i, item) {
+        var pts = [];
+        $.each(item.commits, function(i, c) {
+            pts.push([Date.parse(c.date), c.size, c.size]);
+        });
+        series.push({
+            data: pts,
+            name: item.author
+        });
+    });
+    draw_size_chart(target, series);
+};
+
+function draw_size_chart(div, data) {
+    $(div).highcharts({
+        chart: { type: 'bubble' },
+        title: { text: null },
+        subtitle: { text: null },
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: {
+                hour: '%b %e %H:%M',
+                day: '%b %e',
+                week: '%b %e'
+            },
+        },
+        series: data
+    });
+};
+
 function complexity_plot(target, data) {
     var points = [];
     $.each(data, function(i, item) {
@@ -38,8 +71,13 @@ function draw_complexity_chart(div, data) {
     });
 };
 
-$(document).ready(function() {
-  $.getJSON('/data/complexity.json', function(data) {
-    complexity_plot('#complexity_trend', data);
+function draw_chart(filename, target_div, charting_function) {
+  $.getJSON("/data/" + filename + ".json", function(data) {
+    charting_function(target_div, data);
   });
+};
+
+$(document).ready(function() {
+    draw_chart('commit_sizes', '#commit_sizes', sizes_plot);
+    draw_chart('complexity', '#complexity_trend', complexity_plot);
 });
