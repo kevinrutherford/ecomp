ef05f88e21afe2d9b61741131bcf368956ceace1
Kevin Rutherford <kevin@rutherford-software.com>
2014-07-10 13:49:32 +0100

    Basic churn vs complexity chart

--- public/chart.js
+++ public/chart.js
@@ -31,43 +31,29 @@ function draw_size_chart(div, data) {
     });
 };
 
-function complexity_plot(target, data) {
+function churn_vs_complexity_plot(target, data) {
     var points = [];
     $.each(data, function(i, item) {
         points.push({
-            x: Date.parse(item.date),
-            y: item.complexity,
-            radius: 2 * (item.complexity + 1)
+            x: item.complexity.emaxcc,
+            y: item.churn,
+            name: item.filename
         });
     });
-    draw_complexity_chart(target, points);
+    draw_churn_vs_complexity_chart(target, points);
 };
 
-function draw_complexity_chart(div, data) {
+function draw_churn_vs_complexity_chart(div, data) {
     $(div).highcharts({
-        chart: { type: 'spline' },
+        chart: { type: 'scatter' },
         title: { text: null },
         subtitle: { text: null },
-        xAxis: {
-            type: 'datetime',
-            dateTimeLabelFormats: {
-                hour: '%b %e %H:%M',
-                day: '%b %e',
-                week: '%b %e'
-            },
-        },
+        xAxis: { title: { text: 'Complexity' } },
         yAxis: {
-            title: { text: 'Complexity' },
+            title: { text: 'Churn' },
             min: 0
         },
-        tooltip: {
-            headerFormat: '<b>{series.name}</b><br>',
-            pointFormat: '{point.x:%e-%b}: {point.y:.2f}'
-        },
-        series: [{
-            name: 'Complexity',
-            data: data
-        }]
+        series: [{ data: data }]
     });
 };
 
@@ -79,5 +65,5 @@ function draw_chart(filename, target_div, charting_function) {
 
 $(document).ready(function() {
     draw_chart('commit_sizes', '#commit_sizes', sizes_plot);
-    draw_chart('complexity', '#complexity_trend', complexity_plot);
+    draw_chart('churn_vs_complexity', '#churn_vs_complexity', churn_vs_complexity_plot);
 });
