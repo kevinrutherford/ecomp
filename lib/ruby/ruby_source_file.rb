6434ea3f5c5235c75270e972d7d37712096c22d1
Kevin Rutherford <kevin@rutherford-software.com>
2014-07-20 20:13:54 +0100

    For Ruby, count branches and dependecies

--- lib/ruby/ruby_source_file.rb
+++ lib/ruby/ruby_source_file.rb
@@ -8,12 +8,12 @@ class RubySourceFile
 
   def complexity
     ast = RubyParser.for_current_ruby.parse(@source_code)
-    @methods = []
+    @num_branches = 0
     @num_dependencies = 0
     process_ast(ast)
     {
       filename: @path,
-      functions: @methods,
+      num_branches: @num_branches,
       num_dependencies: @num_dependencies
     }
   end
@@ -21,47 +21,28 @@ class RubySourceFile
   private
 
   def process_ast(node)
-    return 0 unless has_children?(node)
-    if is_method?(node)
-      my = 1 + process_children(node)
-      @methods << {
-        name: node[1],
-        complexity: my
-      }
-      my
-    else
-      @num_dependencies = @num_dependencies + 1 if is_require?(node)
-      comp = process_children(node)
-      comp = comp + 1 if is_branch_point?(node)
-      comp
-    end
+    return unless node
+    @num_dependencies = @num_dependencies + 1 if is_require?(node)
+    @num_branches = @num_branches + 1 if is_branch_point?(node)
+    return unless has_children?(node)
+    node[1..-1].select {|n| n }.each {|n| process_ast(n) }
   end
 
   def has_children?(node)
-    node.is_a?(Array) && node[0] != :lit
+    node && node.is_a?(Array) && node[0] != :lit
   end
 
   def is_branch_point?(node)
-    complex_nodes.include?(node[0])
-  end
-
-  def is_method?(node)
-    [:defn, :defs].include?(node[0])
+    branch_nodes.include?(node[0])
   end
 
   def is_require?(node)
     node[0] == :call && node[2] == :require
   end
 
-  def process_children(node)
-    return 0 unless node && node.size > 1
-    node[1..-1].select {|n| n }.map {|n| process_ast(n) }.inject(:+)
-  end
-
-  def complex_nodes
+  def branch_nodes
     [
-      :defn, :defs,
-      :if, :case, :when,
+      :if, :when,
       :and, :or,
       :for, :while, :until,
       :rescue, :ensure,
