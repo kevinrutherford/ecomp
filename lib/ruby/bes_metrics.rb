223c7890982ba7e789f379045706d7cf51f6eade
Kevin Rutherford <kevin@rutherford-software.com>
2014-08-05 20:49:59 +0100

    Move summarising into the summary objects

--- lib/ruby/bes_metrics.rb
+++ lib/ruby/bes_metrics.rb
@@ -15,13 +15,12 @@ class BesMetrics
   def collect
     @repo.reset
     revisions = @repo.all_revisions_oldest_first
-    summaries = revisions.map {|rev| update_with_complexity(rev) }
+    summaries = revisions.map {|rev| RevisionSummary.new(rev, @repo, @glob) }
     summaries = record_complexity_deltas(summaries)
     @report.update('current_files', CurrentHotspotsReport.new(@repo, @glob))
     @report.update('commits', ComplexityTrendReport.new(summaries))
     @report.update('recent_commits_by_author', DeveloperBehaviourReport.new(summaries))
     @repo.reset
-    $stderr.puts ''
   end
 
   private
@@ -32,24 +31,6 @@ class BesMetrics
       summary.set_delta(delta)
     end
     summaries
-  end
-
-  def update_with_complexity(rev)
-    files = @repo.files_in_revision(rev, @glob)
-    rev[:complexity] = summarise_all_files(files)
-    $stderr.print '.'
-    RevisionSummary.new(rev)
-  end
-
-  def summarise_all_files(files)
-    file_reports = files.map(&:complexity_report)
-    weights = file_reports.empty? ? [0] : file_reports.map {|rpt| rpt['weight'] }
-    weight_sum = weights.inject(:+)
-    {
-      sum_of_file_weights: weight_sum,
-      max_of_file_weights: weights.max,
-      mean_of_file_weights: (weight_sum.to_f / weights.length).round(2)
-    }
   end
 
 end
--- lib/ruby/revision_summary.rb
+++ lib/ruby/revision_summary.rb
@@ -1,14 +1,19 @@
 class RevisionSummary
 
-  def initialize(rev)
+  def initialize(rev, repo, glob)
     @rev = rev
+    @repo = repo
+    @glob = glob
+    @summarised = false
   end
 
   def sum_of_file_weights
+    summarise
     @rev[:complexity][:sum_of_file_weights]
   end
 
   def set_delta(delta)
+    summarise
     @rev[:complexity][:delta_sum_of_file_weights] = delta
   end
 
@@ -21,7 +26,28 @@ class RevisionSummary
   end
 
   def raw_data
+    summarise
     @rev
+  end
+
+  private
+
+  def summarise
+    return if @summarised
+    files = @repo.files_in_revision(@rev, @glob)
+    @rev[:complexity] = summarise_all_files(files)
+    @summarised = true
+  end
+
+  def summarise_all_files(files)
+    file_reports = files.map(&:complexity_report)
+    weights = file_reports.empty? ? [0] : file_reports.map {|rpt| rpt['weight'] }
+    weight_sum = weights.inject(:+)
+    {
+      sum_of_file_weights: weight_sum,
+      max_of_file_weights: weights.max,
+      mean_of_file_weights: (weight_sum.to_f / weights.length).round(2)
+    }
   end
 
 end
