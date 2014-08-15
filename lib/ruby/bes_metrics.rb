89208419e2c8d52ab85fe79c5ca75e656fa3280b
root <root@pal.sandbox>
2014-08-15 17:10:28 +0100

    Tidy up

--- lib/ruby/bes_metrics.rb
+++ lib/ruby/bes_metrics.rb
@@ -15,31 +15,18 @@ class BesMetrics
   end
 
   def collect
+    find_and_analyse_unanalysed_revisions
     @repo.reset
-    revisions = find_revisions_not_already_analysed
-
-    if revisions.empty?
-      puts "Latest revision already analysed"
-    else
-      # TODO append to JSON file (rather than blitz)
-      # TODO extract file writing to DAO
-      summaries = revisions.map {|rev| RevisionSummary.new(rev, @repo, @glob) }
-      summaries = record_complexity_deltas(summaries)
-      # @report.update('current_files', CurrentHotspotsReport.new(@repo, @glob))
-      @report.update('commits', ComplexityTrendReport.new(summaries))
-      # @report.update('recent_commits_by_author', DeveloperBehaviourReport.new(summaries))
-    end
-
-    @repo.reset
+    @report.update('recent_commits_by_author', DeveloperBehaviourReport.new(get_all_summaries_from_metrics))
+    @report.update('current_files', CurrentHotspotsReport.new(@repo, @glob))
   end
 
-  def new_collect
-    @repo.reset
+  private
 
+  def find_and_analyse_unanalysed_revisions
+    @repo.reset
     latest_revision_metrics = @metrics_dao.get_latest_revision_metrics
     revision = find_oldest_unanalysed_revision(latest_revision_metrics)
-
-    i = 0
 
     while revision
       if revision.nil?
@@ -52,22 +39,8 @@ class BesMetrics
       @repo.reset
       latest_revision_metrics = @metrics_dao.get_latest_revision_metrics
       revision = find_oldest_unanalysed_revision(latest_revision_metrics)
-
-      i = i + 1
-      if (i == 5)
-        break
-      end
     end
-
-    summaries = Array.new
-    @metrics_dao.get_all_revision_metrics.each do |metrics|
-      summaries << RevisionSummaryFromMetrics.new(metrics)
-    end
-    @report.update('recent_commits_by_author', DeveloperBehaviourReport.new(summaries))
-    @report.update('current_files', CurrentHotspotsReport.new(@repo, @glob))
   end
-
-  private
 
   def find_oldest_unanalysed_revision(latest_revision_metrics)
     all_revisions_oldest_first = @repo.all_revisions_oldest_first
@@ -93,46 +66,18 @@ class BesMetrics
     revision
   end
 
-
-  def find_revisions_not_already_analysed
-    last_analysed_commit = @metrics_dao.last_commit
-
-    if last_analysed_commit.nil?
-      revisions = @repo.all_revisions_oldest_first
-    else
-      last_found = false
-      revisions = Array.new
-      @repo.all_revisions_oldest_first.each do |commit|
-        if last_found
-          revisions << commit
-        end
-        last_found = true if commit[:ref] == last_analysed_commit
-      end
-    end
-
-    return revisions
-  end
-
-  def record_complexity_deltas(summaries)
-    startTime = Time.now
-    totalSummaries = summaries.length
-    summaries.each_with_index do |summary, i|
-      start = Time.now
-      puts ("Summary (" + (i+1).to_s + " / " + totalSummaries.to_s + ")")
-      delta = summary.sum_of_file_weights - (i > 0 ? summaries[i-1].sum_of_file_weights : 0)
-      summary.set_delta(delta)
-      timeTaken = Time.now - start
-      puts ("Summary completed in " + ('%.2f' % timeTaken.to_s) + "s Estimated completion time: " + ('%.2f' % (timeTaken * (totalSummaries - i))).to_s + "s" )
-    end
-    puts("Total time taken recording complexity deltas: " + ('%.2f' % (Time.now - startTime)) + "s")
-    summaries
-  end
-
-
   def record_complexity_delta(summary, latest_revision_metrics)
     delta = summary.sum_of_file_weights - (latest_revision_metrics.nil? ? 0 : latest_revision_metrics['complexity']['sum_of_file_weights'])
     summary.set_delta(delta)
     summary
+  end
+
+  def get_all_summaries_from_metrics
+    summaries = Array.new
+    @metrics_dao.get_all_revision_metrics.each do |metrics|
+      summaries << RevisionSummaryFromMetrics.new(metrics)
+    end
+    summaries
   end
 
 end
--- lib/ruby/metrics_dao.rb
+++ lib/ruby/metrics_dao.rb
@@ -1,37 +1,13 @@
 require_relative 'json_storage_file'
 require 'json'
 
-
+# TODO migrate recording of hot spots and developer behaviour reports to here?
 class MetricsDAO
 
   def initialize(folder)
     @folder = folder
-    @commits = JSONStorageFile.new(full_path('commits'))
+    @commits = JSONStorageFile.new("#{@folder}/commits.json")
   end
-
-  # TODO delete from here....
-
-  def get_all_commits
-    if not file_exists('commits')
-      return
-    end
-    commits = load_json_file('commits')
-    if (commits.size < 1)
-      return
-    end
-    revision_numbers = Array.new
-    commits.each { |commit| revision_numbers.push(commit['ref']) }
-    revision_numbers
-  end
-
-  def last_commit
-    all_commits = get_all_commits
-    if not all_commits.nil?
-      get_all_commits.last
-    end
-  end
-
-  # TODO ....to here
 
   def get_all_revision_metrics
     @commits.get_content
@@ -44,24 +20,6 @@ class MetricsDAO
   def add_revision_summary(revision_summary)
     @commits.get_content << revision_summary.raw_data
     @commits.write_file
-  end
-
-  # TODO add_file
-
-  private
-
-  def file_exists(filename)
-    File.file?(full_path(filename))
-  end
-
-  def full_path(filename)
-    "#{@folder}/#{filename}.json"
-  end
-
-  def load_json_file(filename)
-    File.open(full_path(filename), "r") do |f|
-      return JSON.load(f)
-    end
   end
 
 end
