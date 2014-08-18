71ac3ba61d6d409dcc141b0b62f1616a2b4922b8
Ian Gilham <ian.gilham@bbc.co.uk>
2014-08-18 15:07:05 +0100

    added java batch support, including a new java metric jar

--- lib/ruby/file_batch.rb
+++ lib/ruby/file_batch.rb
@@ -1,4 +1,5 @@
 require_relative 'javascript_source_batch'
+require_relative 'java_source_batch'
 
 class FileBatch
   def initialize(files, repo)
@@ -14,19 +15,30 @@ class FileBatch
   private
   def process_batch
     jsfilenames = @files.select { |file| file.path =~ /.js$/ }.map { |file| file.path }
-    jsresult = process_javascript(jsfilenames)
-    # javaresult = ...
+    if not jsfilenames.empty?
+      jsresult = process_javascript(jsfilenames)
+      @batchresults.merge!(jsresult)
+    end
+    
+    javafilenames = @files.select { |file| file.path =~ /.java$/ }.map { |file| file.path }
+    if not javafilenames.empty?
+      javaresult = process_java(javafilenames)
+      @batchresults.merge!(javaresult)
+    end
     # rubyresult = ...
     # objcresult = ...
-
-    @batchresults.merge!(jsresult)
-    # @batchresults.merge!(javaresult)
+    
     # @batchresults.merge!(rubyresult)
     # @batchresults.merge!(objcresult)
   end
 
   def process_javascript(files)
     batch = JavaScriptSourceBatch.new(files)
+    batch.process
+  end
+  
+  def process_java(files)
+    batch = JavaSourceBatch.new(files)
     batch.process
   end
 end
